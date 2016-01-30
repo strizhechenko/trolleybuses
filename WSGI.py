e4f6e3b9d662a843a236c8a7d547b1c03a49b4fd
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2016-01-30 18:30:36 +0500

    lol

--- WSGI.py
+++ WSGI.py
@@ -4,5 +4,4 @@ from __init__ import app
 app.debug = False
 
 if __name__ == "__main__":
-    # app.run(port=os.environ.get('PORT', 5000))
-    app.run(port=80)
+    app.run(port=os.environ.get('PORT', 5000))
