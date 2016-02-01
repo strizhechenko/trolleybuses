aa101a88e5269a00d296d3435bfda4e6f17bc480
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2016-02-01 12:05:15 +0500

    Передаём список остановок в темплейт

--- __init__.py
+++ __init__.py
@@ -10,31 +10,54 @@ from markdown import Markdown
 app = Flask(__name__)
 
 STATIONS = {
-    u'шварца-работа': 'http://m.ettu.ru/station/962909',
-    u'дом-шварца': 'http://m.ettu.ru/station/962512',
-    u'гостиница уктус': 'http://m.ettu.ru/station/962907',
-    u'шварца-дом': 'http://m.ettu.ru/station/961948',
+    u'шварца-работа': {
+        'url': 'http://m.ettu.ru/station/962909',
+        'numbers': [6, 20]
+    },
+    u'дом-шварца': {
+        'url': 'http://m.ettu.ru/station/962512',
+        'numbers': [9]
+    },
+    u'гостиница уктус': {
+        'url': 'http://m.ettu.ru/station/962907',
+        'numbers': [6],
+    },
+    u'шварца-дом': {
+        'url': 'http://m.ettu.ru/station/961948',
+        'numbers': [9],
+    },
 }
 
 MARKDOWN = Markdown()
+
 
 @app.route('/')
 def main():
     return render_template('index.html', content=main_content())
 
+
 def main_content():
     """ главная страница """
-    output = []
-    for station, url in STATIONS.items():
-        tree = etree.parse(urlopen(url))
-        output.append("# %s" % (station,))
+    output = {}
+    for station, data in STATIONS.items():
+        tree = etree.parse(urlopen(data['url']))
+        output[station] = {
+            'header': "# %s" % (station,),
+            'buslist': [],
+        }
         root = tree.getroot()
         for bus in root.findall('body/div/p/div'):
             bus_num = bus[0].find('b').text
+            if int(bus_num) not in data['numbers']:
+                continue
             bus_time = bus[1].text
             bus_distance = bus[2].text
-            output.append("- %s / %s / %s" % (bus_num, bus_time, bus_distance))
-    return MARKDOWN.convert(u"\n".join(output))
+            bus_string = "- %s / %s / %s" % (bus_num, bus_time, bus_distance)
+            output[station]['buslist'].append(bus_string)
+
+        output[station]['header'] = MARKDOWN.convert(output[station]['header'])
+        output[station]['buslist'] = MARKDOWN.convert(u'\n'.join(output[station]['buslist']))
+    return output
 
 if __name__ == '__main__':
     app.run()
